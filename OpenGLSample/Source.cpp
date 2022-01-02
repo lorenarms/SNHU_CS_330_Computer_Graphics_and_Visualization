b7f737cc32984257bbd4c91cf9bc8f24f0095698
Lorenarms <lorenzo.joseph.dominguez@gmail.com>
2022-01-01 19:03:06 -0800

    Create repo

--- OpenGLSample/Source.cpp
+++ OpenGLSample/Source.cpp
@@ -51,7 +51,7 @@ int main()
 
 	// glfw window creation
 	// --------------------
-	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Lawrence Artl III", NULL, NULL);
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;

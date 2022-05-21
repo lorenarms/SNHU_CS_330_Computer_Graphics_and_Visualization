7bb722b1a746cca4af3571de786b42f22eab2966
lorenarms <lorenzo.joseph.dominguez@gmail.com>
2022-05-21 10:10:43 -0700

    update for video

--- 5-3 Assignment Texturing a Pyramid/SceneBuilder.cpp
+++ 5-3 Assignment Texturing a Pyramid/SceneBuilder.cpp
@@ -1,3 +1,19 @@
+///
+///	This class is designed to be the one place where all
+///	shapes are created. It utilizes the UShapeBuilder class
+///	to build the shapes into GLMesh structs. The individual
+///	shape structs are then added to a vector called "scene" that is
+///	then passed to the main render method in the source class
+///	(5-3 Texturing a Pyramid.cpp)
+///
+///
+///	Follow the standard shown below in the <summary> to see
+///	how to build a shape
+///
+///	Uncomment other shapes below to see more examples of how to build them
+
+
+
 #include "SceneBuilder.h"
 #include "ShapeBuilder.h"
 #include "Mesh.h"
@@ -10,38 +26,49 @@ void SceneBuilder::UBuildScene(vector<GLMesh>& scene)
 
 	// define the folder location for all textures used
 	// make sure all textures stored in this location
+	// CHANGE THIS to match your own location on your machine
 #define textureFolderLocation "C:\\Users\\Lawrence\\Git Repos\\SNHU_CS_330_Projects\\3-5 Milestone 3D Scene\\textures\\"
 
 // define a concat function that takes the above location and concatenates the texture name together
 #define concat(first, second) first second
 
-		/// <summary>
-///
-///BUILDING A SHAPE:
-///
-///		GLMesh <glmesh_name>
-///		<glmesh_name>.p = {
-///		red float, green float, blue float, alpha float,
-///		x scale float, y scale float, z scale float,
-///		x rotation degrees float, 1.0f, 0.0f, 0.0f,
-///		y rotation degrees float, 0.0f, 1.0f, 0.0f,
-///		z rotation degrees float, 0.0f, 0.0f, 1.0f,
-///		x translate float, y translate float, z translate float,
-///		x texture scale float, y texture scale float
-///		};
-///		#define texture "<texture_name.file_type>"
-///		<glmesh_name>.texFilename = concat(textureFolderLocation, texture);
-///		<glmesh_name>.length = float;
-///		<glmesh_name>.height = float;
-///		<glmesh_name>.radius = float;
-///		<glmesh_name>.number_of_sides = float;
-///		ShapeBuilder::<ShapeBuilderMethod>(<glmesh_name>);
-///		**OR**
-///		ShapeBuilder::<ShapeBuild "Rainbow" Shape>(<glmesh_name>, rand() % 100 + 1);
-///		scene.push_back(<glmesh_name>);
-///
-/// </summary>
-/// <param name="scene"></param>
+	// seed the rand() function once
+	srand(time(nullptr));
+
+
+	/// <summary>
+	///
+	///BUILDING A SHAPE:
+	///To build a shape, follow the following standard:
+	///
+	///		GLMesh <glmesh_name>
+	///		<glmesh_name>.p = {
+	///		red float, green float, blue float, alpha float,
+	///		x scale float, y scale float, z scale float,
+	///		x rotation degrees float, 1.0f, 0.0f, 0.0f,
+	///		y rotation degrees float, 0.0f, 1.0f, 0.0f,
+	///		z rotation degrees float, 0.0f, 0.0f, 1.0f,
+	///		x translate float, y translate float, z translate float,
+	///		x texture scale float, y texture scale float
+	///		};
+	///		#define texture "<texture_name.file_type>"
+	///		<glmesh_name>.texFilename = concat(textureFolderLocation, texture);
+	///		<glmesh_name>.length = float;
+	///		<glmesh_name>.height = float;
+	///		<glmesh_name>.radius = float;
+	///		<glmesh_name>.number_of_sides = float;
+	///		ShapeBuilder::<ShapeBuilderMethod>(<glmesh_name>);
+	///		**OR**
+	///		ShapeBuilder::<ShapeBuild "Rainbow" Shape>(<glmesh_name>, rand() % 100 + 1);
+	///		scene.push_back(<glmesh_name>);
+	///
+	///
+	///
+	///		See below for an example using the UBuildCone() method
+	///
+	///
+	/// </summary>
+	/// <param name="scene"></param>
 
 
 	GLMesh con_gMesh01;
@@ -68,8 +95,7 @@ void SceneBuilder::UBuildScene(vector<GLMesh>& scene)
 
 
 //
-////// seed the rand() function once
-//	srand(time(nullptr));
+
 //		GLMesh gMesh01;
 //	gMesh01.p = {
 //		1.0f, 1.0f, 1.0f, 1.0f,				// color r, g, b a

d823eb4c005e03b17f6b9c6546219fe287740928
lorenarms <lorenzo.joseph.dominguez@gmail.com>
2022-06-01 05:54:07 -0700

    minor update

--- 7-1 Final Project - A 3D Scene/7-1 Final Project - A 3D Scene.cpp
+++ 7-1 Final Project - A 3D Scene/7-1 Final Project - A 3D Scene.cpp
@@ -59,7 +59,7 @@ bool perspective = false;
 
 
 // camera
-Camera gCamera(glm::vec3(-6.0f, 6.0f, -7.0f), glm::vec3(0.0f, 1.0f, 0.0f), 45.0f, -35.0f);
+Camera gCamera(glm::vec3(-6.0f, 6.0f, -7.0f), glm::vec3(0.0f, 1.0f, 0.0f), 60.0f, -35.0f);
 //Camera gCamera(glm::vec3(0.0f, 4.0f, 8.0f));
 
 
